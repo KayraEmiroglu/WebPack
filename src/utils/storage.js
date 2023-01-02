380beebd4a92c9391de120dbec09fb8c4dd04767
Kayra Emiroglu <kemiroglu7@gmail.com>
2023-01-02 21:56:32 +0300

    WebPack

---
+++ src/utils/storage.js
@@ -0,0 +1,7 @@
+const setItem = (key, value) => {
+    localStorage.setItem(key, value);
+  };
+  const getItem = (key) => {
+    return localStorage.getItem(key);
+  };
+  export { setItem, getItem};
