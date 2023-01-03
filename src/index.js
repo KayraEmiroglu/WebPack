4306dda6e8b8090a00591931fc284274c45f9ddd
Kayra Emiroglu <kemiroglu7@gmail.com>
2023-01-03 21:22:39 +0300

    Babel

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ document.getElementById("btnClick").addEventListener("click", () => {
   alert("Hello Webpack");
   alert("v4");
   alert("v2");
+
+  const arr = [123,213,-5,-1312,2,133];
+  console.log(getArray(arr));
+
   setItem(
     "token",
     "klsdjfklsjkdfjksldsdkjfklsjdfklsjdlfkjslkdfjklsfklsjkldfjskljfklsjfl"
   );
 });
+
+
+const getArray =(arr)=>{
+  return arr.filter(x=> x>10).reduce((t, x)=> x+t,0);
+};
+
